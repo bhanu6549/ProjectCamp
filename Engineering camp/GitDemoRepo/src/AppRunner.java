66b9aba2d1feb3ff8b55ec76a223679a35b4837c
M1057752 <M1057752@G1C2ML27939.mindtree.com>
2020-03-19 15:11:48 +0530

    for loop add

--- Engineering camp/GitDemoRepo/src/AppRunner.java
+++ Engineering camp/GitDemoRepo/src/AppRunner.java
@@ -1,7 +1,12 @@
+import java.util.Iterator;
 
 public class AppRunner {
 	public static void main(String[] args) {
-		System.out.println("Hii");
+		for (int i = 0; i < args.length; i++) {
+			
+		
+		System.out.println();
+		}
 		
 	}
 
