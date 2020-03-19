3cd044ee9049a9800b6a301c4a0a6afa190ae5d2
Bhanu pratap <33762507+bhanu6549@users.noreply.github.com>
2020-03-19 15:13:34 +0530

    Update AppRunner.java

--- Engineering camp/GitDemoRepo/src/AppRunner.java
+++ Engineering camp/GitDemoRepo/src/AppRunner.java
@@ -2,11 +2,7 @@ import java.util.Iterator;
 
 public class AppRunner {
 	public static void main(String[] args) {
-		for (int i = 0; i < args.length; i++) {
-			
 		
-		System.out.println();
-		}
 		
 	}
 
